bedf3b866ac3edf628399ec4f43a19ce47d64bf2
Anna Host <roromia@mail.ru>
2021-08-23 16:02:14 +0300

    add second test function

--- test_main_page.py
+++ test_main_page.py
@@ -7,4 +7,5 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()                      # открываем страницу
     # выполняем метод страницы — переходим на страницу логина
+    page.should_be_login_link()
     page.go_to_login_page()
