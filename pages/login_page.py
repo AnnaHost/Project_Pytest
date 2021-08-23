fb4e3af631fa522009ff3508f18b2c988d8d93c8
Anna Host <roromia@mail.ru>
2021-08-23 18:07:35 +0300

    add login page

---
+++ pages/login_page.py
@@ -0,0 +1,25 @@
+from selenium.webdriver.common.by import By
+from .locators import LoginPanelLocators
+from .base_page import BasePage
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        assert self.browser.current_url(
+            "http://selenium1py.pythonanywhere.com/accounts/login/"), "Текущая страница некорректная"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(
+            *LoginPanelLocators.LOGIN_LINK), "Отсутствует форма логина"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(
+            *LoginPanelLocators.REGISTRATION_LINK), "Отсутствует форма регистрации"
--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,18 @@
+from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
+import time
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+    link = "http://selenium1py.pythonanywhere.com/"
+    link2 = "http://selenium1py.pythonanywhere.com/pl/accounts/login/"
     # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page = MainPage(browser, link)
-    page.open()                      # открываем страницу
-    # выполняем метод страницы — переходим на страницу логина
-    page.should_be_login_link()
-    page.go_to_login_page()
+    # page = MainPage(browser, link)
+    # page.open()                      # открываем страницу
+    # # выполняем метод страницы — переходим на страницу логина
+    # page.should_be_login_link()
+    # page.go_to_login_page()
+    page2 = LoginPage(browser, link2)
+    page2.open()
+    time.sleep(15)
+    page2.should_be_login_page()
